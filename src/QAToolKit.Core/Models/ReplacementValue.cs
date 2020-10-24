d7c814da4e5a7e7a928f981d387c6d98a3d22906
mihaj <miha.jakovac@gmail.com>
2020-10-24 14:06:07 +0200

    new icon and ReplacementValue model

---
+++ src/QAToolKit.Core/Models/ReplacementValue.cs
@@ -0,0 +1,11 @@
+﻿namespace QAToolKit.Core.Models
+{
+    /// <summary>
+    /// Key value pair for replacing a placeholder with another value
+    /// </summary>
+    public class ReplacementValue
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
