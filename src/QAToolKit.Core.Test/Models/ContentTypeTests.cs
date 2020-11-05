dfcd2feac04b57b149a32bdbddd4a0bb69f9209f
Miha Jakovac <miha.jakovac@gmail.com>
2020-11-05 08:42:32 +0100

    Release to main branch v0.2.0 (#5)
    * Mj updates (#1)
    
    * property model updated to support nested propterties and their values
    
    * httprequest refactoring, content type, more unit tests
    
    * Create codeql-analysis.yml
    
    * Update codeql-analysis.yml
    
    * Before alpha release updates (#4)
    
    * Http Request object update
    
    * content type has enumerator, added data generator and data replacement logic to manipulate HttpRequest object
    
    * missing xml comments added
    
    * new content type added application/octet-stream
    
    * readme file update
    
    * data replacer initial commit, more tests, refactorings
    
    * sync tests signature fix
    
    * generate url parameters fix
    
    * data replacer v1
    
    * http tools refactorings
    
    * alpha 1
    
    * readme file updates
    
    * full json payload replacement
    
    * path type compare fixed for URL path replacement
    
    * Custom exception implemented
    
    * cleanup
    
    * removed unnecessary comments

--- src/QAToolKit.Core.Test/Models/ContentTypeTests.cs
+++ src/QAToolKit.Core.Test/Models/ContentTypeTests.cs
@@ -7,42 +7,55 @@ namespace QAToolKit.Core.Test.Models
     public class ContentTypeTests
     {
         [Fact]
-        public void IntegrationTestTypesPresent_Success()
+        public void ContentTypeJsonTestsPresent_Success()
         {
             Assert.Equal("application/json", ContentType.Json.Value());
         }
 
         [Fact]
-        public void LoadTestTypesPresent_Success()
+        public void ContentTypeXmlTestsPresent_Success()
         {
             Assert.Equal("application/xml", ContentType.Xml.Value());
         }
 
         [Fact]
-        public void SqlTestTypesPresent_Success()
+        public void ContentTypeFormTestsPresent_Success()
         {
             Assert.Equal("application/x-www-form-urlencoded", ContentType.FormUrlEncoded.Value());
         }
 
+        [Fact]
+        public void ContentTypeBinaryTestsPresent_Success()
+        {
+            Assert.Equal("application/octet-stream", ContentType.OctetStream.Value());
+        }
+
         [Theory]
         [InlineData("application/json")]
-        public void ConverJsontFromString_Success(string value)
+        public void ConverJsonFromString_Success(string value)
         {
-            Assert.Equal(ContentType.Json, ContentType.FromString(value));
+            Assert.Equal(ContentType.Json, ContentType.From(value));
         }
 
         [Theory]
         [InlineData("application/xml")]
         public void ConvertXmlFromString_Success(string value)
         {
-            Assert.Equal(ContentType.Xml, ContentType.FromString(value));
+            Assert.Equal(ContentType.Xml, ContentType.From(value));
         }
 
         [Theory]
         [InlineData("application/x-www-form-urlencoded")]
         public void ConvertFormFromString_Success(string value)
         {
-            Assert.Equal(ContentType.FormUrlEncoded, ContentType.FromString(value));
+            Assert.Equal(ContentType.FormUrlEncoded, ContentType.From(value));
+        }
+
+        [Theory]
+        [InlineData("application/octet-stream")]
+        public void ConvertBinaryFromString_Success(string value)
+        {
+            Assert.Equal(ContentType.OctetStream, ContentType.From(value));
         }
 
         [Theory]
@@ -51,7 +64,107 @@ namespace QAToolKit.Core.Test.Models
         [InlineData("application/pdf")]
         public void ConvertFromString_Fails(string value)
         {
-            Assert.Throws<Exception>(() => ContentType.FromString(value));
+            Assert.Throws<Exception>(() => ContentType.From(value));
+        }
+
+        [Theory]
+        [InlineData(ContentType.Enumeration.Json)]
+        public void ConverJsonFromEnum_Success(ContentType.Enumeration value)
+        {
+            Assert.Equal(ContentType.Json, ContentType.From(value));
+        }
+
+        [Theory]
+        [InlineData(ContentType.Enumeration.Xml)]
+        public void ConvertXmlFromEnum_Success(ContentType.Enumeration value)
+        {
+            Assert.Equal(ContentType.Xml, ContentType.From(value));
+        }
+
+        [Theory]
+        [InlineData(ContentType.Enumeration.FormUrlEncoded)]
+        public void ConvertFormFromEnum_Success(ContentType.Enumeration value)
+        {
+            Assert.Equal(ContentType.FormUrlEncoded, ContentType.From(value));
+        }
+
+        [Theory]
+        [InlineData(ContentType.Enumeration.OctetStream)]
+        public void ConvertBinaryFromEnum_Success(ContentType.Enumeration value)
+        {
+            Assert.Equal(ContentType.OctetStream, ContentType.From(value));
+        }
+
+        [Fact]
+        public void ConvertJsonObjectToString_Success()
+        {
+            Assert.Equal("application/json", ContentType.Json.Value());
+        }
+
+        [Fact]
+        public void ConvertXmlObjectToString_Success()
+        {
+            Assert.Equal("application/xml", ContentType.Xml.Value());
+        }
+
+        [Fact]
+        public void ConvertFormObjectToString_Success()
+        {
+            Assert.Equal("application/x-www-form-urlencoded", ContentType.FormUrlEncoded.Value());
+        }
+
+        [Fact]
+        public void ConvertBinaryObjectToString_Success()
+        {
+            Assert.Equal("application/octet-stream", ContentType.OctetStream.Value());
+        }
+
+        [Fact]
+        public void ConvertJsonObjectToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.Json, ContentType.ToEnum(ContentType.Json));
+        }
+
+        [Fact]
+        public void ConvertXmlObjectToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.Xml, ContentType.ToEnum(ContentType.Xml));
+        }
+
+        [Fact]
+        public void ConvertFormObjectToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.FormUrlEncoded, ContentType.ToEnum(ContentType.FormUrlEncoded));
+        }
+
+        [Fact]
+        public void ConvertBinaryObjectToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.OctetStream, ContentType.ToEnum(ContentType.OctetStream));
+        }
+
+        [Fact]
+        public void ConvertJsonStringToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.Json, ContentType.ToEnum("application/json"));
+        }
+
+        [Fact]
+        public void ConvertXmlStringToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.Xml, ContentType.ToEnum("application/xml"));
+        }
+
+        [Fact]
+        public void ConvertFormStringToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.FormUrlEncoded, ContentType.ToEnum("application/x-www-form-urlencoded"));
+        }
+
+        [Fact]
+        public void ConvertBinaryStringToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.OctetStream, ContentType.ToEnum("application/octet-stream"));
         }
     }
 }
---
+++ src/QAToolKit.Core/HttpRequestTools/DataReplacementOptions.cs
@@ -0,0 +1,28 @@
+﻿using QAToolKit.Core.Models;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("QAToolKit.Core.Test")]
+namespace QAToolKit.Core.HttpRequestTools
+{
+    /// <summary>
+    /// HttpRequest Data replacer options
+    /// </summary>
+    public class DataReplacementOptions
+    {
+        /// <summary>
+        /// Key/value pairs of replacement values
+        /// </summary>
+        internal ReplacementValue[] ReplacementValues { get; private set; }
+
+        /// <summary>
+        /// Use replacement values
+        /// </summary>
+        /// <param name="replacementValues"></param>
+        /// <returns></returns>
+        public DataReplacementOptions AddReplacementValues(ReplacementValue[] replacementValues)
+        {
+            ReplacementValues = replacementValues;
+            return this;
+        }
+    }
+}
