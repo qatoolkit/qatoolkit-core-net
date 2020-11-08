63cec3f416e6488955fdca914863104f6e95adc5
mihaj <miha.jakovac@gmail.com>
2020-11-08 10:15:03 +0100

    exception update

--- src/QAToolKit.Core/Exceptions/QAToolKitCoreException.cs
+++ src/QAToolKit.Core/Exceptions/QAToolKitCoreException.cs
@@ -3,16 +3,29 @@ using System.Runtime.Serialization;
 
 namespace QAToolKit.Core.Exceptions
 {
+    /// <summary>
+    /// QA Toolkit core exception
+    /// </summary>
+    [Serializable]
     public class QAToolKitCoreException : Exception
     {
+        /// <summary>
+        /// QA Toolkit core exception
+        /// </summary>
         public QAToolKitCoreException(string message) : base(message)
         {
         }
 
+        /// <summary>
+        /// QA Toolkit core exception
+        /// </summary>
         public QAToolKitCoreException(string message, Exception innerException) : base(message, innerException)
         {
         }
 
+        /// <summary>
+        /// QA Toolkit core exception
+        /// </summary>
         protected QAToolKitCoreException(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
