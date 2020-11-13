fab69114717ea2b3c9b7515358b719cfb7d117f1
Miha Jakovac <miha.jakovac@gmail.com>
2020-11-13 18:51:35 +0100

    net 5 support

---
+++ src/QAToolKit.Core.Test/Exceptions/QAToolKitCoreExceptionTests.cs
@@ -0,0 +1,27 @@
+﻿using QAToolKit.Core.Exceptions;
+using System;
+using Xunit;
+
+namespace QAToolKit.Core.Test.Exceptions
+{
+    public class QAToolKitCoreExceptionTests
+    {
+        [Fact]
+        public void CreateExceptionTest_Successful()
+        {
+            var exception = new QAToolKitCoreException("my error");
+
+            Assert.Equal("my error", exception.Message);
+        }
+
+        [Fact]
+        public void CreateExceptionWithInnerExceptionTest_Successful()
+        {
+            var innerException = new Exception("Inner");
+            var exception = new QAToolKitCoreException("my error", innerException);
+
+            Assert.Equal("my error", exception.Message);
+            Assert.Equal("Inner", innerException.Message);
+        }
+    }
+}
