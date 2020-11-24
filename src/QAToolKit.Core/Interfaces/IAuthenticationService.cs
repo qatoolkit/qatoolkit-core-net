1624bfe46cdd688838d3ef9849e7d44e093ed485
Miha Jakovac <miha.jakovac@gmail.com>
2020-11-24 08:48:10 +0100

    Database test interfaces, maintenance

---
+++ src/QAToolKit.Core/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace QAToolKit.Core.Interfaces
+{
+    /// <summary>
+    /// Authentication service interface
+    /// </summary>
+    public interface IAuthenticationService
+    {
+        /// <summary>
+        /// Get JWT access token
+        /// </summary>
+        /// <returns></returns>
+        Task<string> GetAccessToken();
+    }
+}
---
+++ src/QAToolKit.Core/Interfaces/IDatabaseTestGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace QAToolKit.Core.Interfaces
+{
+    /// <summary>
+    /// Database script generator interface
+    /// </summary>
+    public interface IDatabaseTestGenerator<T>
+    {
+        /// <summary>
+        /// Generate a script
+        /// </summary>
+        /// <returns></returns>
+        Task<IEnumerable<T>> Generate();
+    }
+}
