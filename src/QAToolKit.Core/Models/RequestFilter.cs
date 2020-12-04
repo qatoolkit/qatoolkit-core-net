0f04ad02d8c7fcb0447b38f3ad433429fb8a4cbd
Miha Jakovac <miha.jakovac@gmail.com>
2020-12-04 17:22:40 +0100

    HttpMethodsWhitelist and GeneralContains request filters added

--- src/QAToolKit.Core/Models/RequestFilter.cs
+++ src/QAToolKit.Core/Models/RequestFilter.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Net.Http;
 
 namespace QAToolKit.Core.Models
 {
@@ -8,16 +9,24 @@ namespace QAToolKit.Core.Models
     public class RequestFilter
     {
         /// <summary>
-        /// A list of endpoint names which are allowed to be processed.
+        /// Filter requests by a list of endpoint names which are allowed to be processed.
         /// </summary>
         public string[] EndpointNameWhitelist { get; set; }
         /// <summary>
-        /// A list of allowed authentication types
+        /// Filter requests by a list of allowed authentication types. AuthenticationType is a tag or a placeholder you can place in the Swagger endpoint description.
         /// </summary>
         public IEnumerable<AuthenticationType.Enumeration> AuthenticationTypes { get; set; }
         /// <summary>
-        /// A list of allowed test types
+        /// Filter requests by a list of allowed test types. TestType is a tag or a placeholder you can place in the Swagger endpoint description.
         /// </summary>
         public IEnumerable<TestType.Enumeration> TestTypes { get; set; }
+        /// <summary>
+        /// Filter requests by a list of HTTP methods whiltelist.
+        /// </summary>
+        public IEnumerable<HttpMethod> HttpMethodsWhitelist { get; set; }
+        /// <summary>
+        /// Filter requests by a list of strings which can be found in swagger description, summary or tags. String case is ignored.
+        /// </summary>
+        public IEnumerable<string> GeneralContains { get; set; }
     }
 }
