dca752b95ab0367b1eff1b08ef048257f1087f36
Miha Jakovac <miha.jakovac@gmail.com>
2020-12-09 07:47:12 +0100

    MultipartFormData and TextPlain content types added

---
+++ src/QAToolKit.Core.Test/Models/ContentType/ContentTypeFormTests.cs
@@ -0,0 +1,52 @@
+﻿using QAToolKit.Core.Models;
+using Xunit;
+
+namespace QAToolKit.Core.Test.Models
+{
+    public class ContentTypeFormTests
+    {
+        [Fact]
+        public void ContentTypeFormTestsPresent_Success()
+        {
+            Assert.Equal("application/x-www-form-urlencoded", ContentType.FormUrlEncoded.Value());
+        }
+
+        [Theory]
+        [InlineData("application/x-www-form-urlencoded")]
+        public void ConvertFormFromString_Success(string value)
+        {
+            Assert.Equal(ContentType.FormUrlEncoded, ContentType.From(value));
+        }
+
+        [Theory]
+        [InlineData(ContentType.Enumeration.FormUrlEncoded)]
+        public void ConvertFormFromEnum_Success(ContentType.Enumeration value)
+        {
+            Assert.Equal(ContentType.FormUrlEncoded, ContentType.From(value));
+        }
+
+        [Fact]
+        public void ConvertFormObjectToString_Success()
+        {
+            Assert.Equal("application/x-www-form-urlencoded", ContentType.FormUrlEncoded.Value());
+        }
+
+        [Fact]
+        public void ConvertFormObjectToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.FormUrlEncoded, ContentType.ToEnum(ContentType.FormUrlEncoded));
+        }
+
+        [Fact]
+        public void ConvertFormStringToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.FormUrlEncoded, ContentType.ToEnum("application/x-www-form-urlencoded"));
+        }
+
+        [Fact]
+        public void ConvertFormStringToContentType_Success()
+        {
+            Assert.Equal(ContentType.FormUrlEncoded, ContentType.From("application/x-www-form-urlencoded"));
+        }
+    }
+}
