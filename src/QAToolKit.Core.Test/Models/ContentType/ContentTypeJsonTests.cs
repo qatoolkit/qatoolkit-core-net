87a914509eb8e508ea86ab394549441467429727
Miha Jakovac <miha.jakovac@gmail.com>
2020-12-09 08:04:08 +0100

    MultipartFormData and TextPlain content types added
    * MultipartFormData and TextPlain content types added
    
    * version bump

---
+++ src/QAToolKit.Core.Test/Models/ContentType/ContentTypeJsonTests.cs
@@ -0,0 +1,71 @@
+﻿using QAToolKit.Core.Exceptions;
+using QAToolKit.Core.Models;
+using Xunit;
+
+namespace QAToolKit.Core.Test.Models
+{
+    public class ContentTypeJsonTests
+    {
+        [Fact]
+        public void ContentTypeJsonTestsPresent_Success()
+        {
+            Assert.Equal("application/json", ContentType.Json.Value());
+        }
+
+        [Theory]
+        [InlineData("application/json")]
+        public void ConverJsonFromString_Success(string value)
+        {
+            Assert.Equal(ContentType.Json, ContentType.From(value));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("somestring")]
+        public void ConvertFromString_Fails(string value)
+        {
+            Assert.Throws<QAToolKitCoreException>(() => ContentType.From(value));
+        }
+
+        [Theory]
+        [InlineData(ContentType.Enumeration.Json)]
+        public void ConverJsonFromEnum_Success(ContentType.Enumeration value)
+        {
+            Assert.Equal(ContentType.Json, ContentType.From(value));
+        }
+
+        [Fact]
+        public void ConvertJsonObjectToString_Success()
+        {
+            Assert.Equal("application/json", ContentType.Json.Value());
+        }
+
+        [Fact]
+        public void ConvertJsonObjectToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.Json, ContentType.ToEnum(ContentType.Json));
+        }
+
+        [Fact]
+        public void ConvertJsonStringToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.Json, ContentType.ToEnum("application/json"));
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        [InlineData("somestring")]
+        public void ConvertStringToEnum_Fails(string value)
+        {
+            Assert.Throws<QAToolKitCoreException>(() => ContentType.ToEnum(value));
+        }
+
+        [Fact]
+        public void ConvertJsonStringToContentType_Success()
+        {
+            Assert.Equal(ContentType.Json, ContentType.From("application/json"));
+        }
+    }
+}
---
+++ src/QAToolKit.Core.Test/Models/ContentType/ContentTypeMultipartTests.cs
@@ -0,0 +1,52 @@
+﻿using QAToolKit.Core.Models;
+using Xunit;
+
+namespace QAToolKit.Core.Test.Models
+{
+    public class ContentTypeMultipartTests
+    {
+        [Fact]
+        public void ContentTypeMultipartTestsPresent_Success()
+        {
+            Assert.Equal("multipart/form-data", ContentType.MultipartFormData.Value());
+        }
+
+        [Theory]
+        [InlineData("multipart/form-data")]
+        public void ConvertMultipartFromString_Success(string value)
+        {
+            Assert.Equal(ContentType.MultipartFormData, ContentType.From(value));
+        }
+
+        [Theory]
+        [InlineData(ContentType.Enumeration.MultipartFormData)]
+        public void ConvertMultipartFromEnum_Success(ContentType.Enumeration value)
+        {
+            Assert.Equal(ContentType.MultipartFormData, ContentType.From(value));
+        }
+
+        [Fact]
+        public void ConvertMultipartObjectToString_Success()
+        {
+            Assert.Equal("multipart/form-data", ContentType.MultipartFormData.Value());
+        }
+
+        [Fact]
+        public void ConvertMultipartObjectToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.MultipartFormData, ContentType.ToEnum(ContentType.MultipartFormData));
+        }
+
+        [Fact]
+        public void ConvertMultipartStringToEnum_Success()
+        {
+            Assert.Equal(ContentType.Enumeration.MultipartFormData, ContentType.ToEnum("multipart/form-data"));
+        }
+
+        [Fact]
+        public void ConvertMultipartStringToContentType_Success()
+        {
+            Assert.Equal(ContentType.MultipartFormData, ContentType.From("multipart/form-data"));
+        }
+    }
+}
