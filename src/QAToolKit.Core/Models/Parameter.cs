1dab218e13c699f25ca1ed72e621ec41a73d5679
Miha Jakovac <miha.jakovac@gmail.com>
2021-10-27 19:31:09 +0200

    format added to Property object, split class (#61)

---
+++ src/QAToolKit.Core/Models/Parameter.cs
@@ -0,0 +1,37 @@
+﻿namespace QAToolKit.Core.Models
+{
+    /// <summary>
+    /// An URL parameter
+    /// </summary>
+    public class Parameter
+    {
+        /// <summary>
+        /// Parameter name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Parameter type like string, integer, number,...
+        /// </summary>
+        public string Type { get; set; }
+        /// <summary>
+        /// Parameter format like int32, int64, double,...
+        /// </summary>
+        public string Format { get; set; }
+        /// <summary>
+        /// Is parameter nullable
+        /// </summary>
+        public bool Nullable { get; set; }
+        /// <summary>
+        /// Parameter value
+        /// </summary>
+        public string Value { get; set; }
+        /// <summary>
+        /// Is parameter required
+        /// </summary>
+        public bool Required { get; set; }
+        /// <summary>
+        /// Parameter location can be either path or in query
+        /// </summary>
+        public Location Location { get; set; }
+    }
+}
